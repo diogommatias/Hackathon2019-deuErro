574c13cef3d7588a4d7365300ec95172d3e6dd8e
Pedro Nunes <nunes.pedroms@gmail.com>
2019-03-29 22:02:02 +0000

    Added home search autofill

--- flash-volunteer/src/app/flash-volunteer/home-search/home-search.component.ts
+++ flash-volunteer/src/app/flash-volunteer/home-search/home-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home-search',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeSearchComponent implements OnInit {
 
-  constructor() { }
+  homeSearchForm: FormGroup;
+
+  options = [
+    'Lisboa',
+    'Porto',
+  ];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.homeSearchForm = this.fb.group({
+      cityName: ""
+    });
   }
 
 }
