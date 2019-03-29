11bcb311905991b803a1723a0c9139941bbb5f16
Pedro Nunes <nunes.pedroms@gmail.com>
2019-03-29 21:17:20 +0000

    Added home-search

--- flash-volunteer/src/app/app.module.ts
+++ flash-volunteer/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './flash-volunteer/home/home.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { HomeSearchComponent } from './flash-volunteer/home-search/home-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HomeSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
